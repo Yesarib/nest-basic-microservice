c036de950de58aa4fea81924157d3591e3d1db27
Barış Yesari <105274476+Yesarib@users.noreply.github.com>
2024-10-18 14:31:20 +0300

    Delete main/dist directory

--- main/dist/product/product.controller.d.ts
+++
@@ -1,8 +0,0 @@
-import { ProductService } from './product.service';
-export declare class ProductController {
-    private productService;
-    constructor(productService: ProductService);
-    all(): Promise<import("./product.model").Product[]>;
-    createProduct(product: any): Promise<void>;
-    updateProduct(product: any): Promise<void>;
-}
